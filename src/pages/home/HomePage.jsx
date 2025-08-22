3f50a2bb52f6c0669e02706821082435adb48838
J-mung <xorb2298@gmail.com>
2025-08-23 02:30:20 +0900

    임시 홈 화면 추가

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+/**
+ * 임시 홈 화면
+ * @returns
+ */
+export default function HomePage() {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>여기에 페이지 콘텐츠를 배치하세요.</p>
+      <div style={{ height: 1200 }} />
+    </div>
+  );
+}
