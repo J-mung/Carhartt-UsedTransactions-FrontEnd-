743a5c5447c7be47680dd63b0b1290a7b4168089
J-mung <xorb2298@gmail.com>
2025-08-24 20:26:58 +0900

    page 의존성 주입 방식으로 수정

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,14 @@
 import MainLayout from '@/app/layouts/MainLayout';
 import HomePage from '@/pages/home/HomePage.jsx';
 
-export default function App() {
+export default function App({
+  Page = HomePage,
+  pageProps = {},
+  layoutProps = { stickyFooter: false },
+}) {
   return (
-    <MainLayout stickyFooter={false}>
-      <HomePage />
+    <MainLayout {...layoutProps}>
+      <Page {...pageProps} />
     </MainLayout>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import App from '@/app/App.jsx';
 import Providers from '@/app/providers.jsx';
+import HomePage from '@/pages/home/HomePage.jsx';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Providers>
-    <App />
+    <App page={HomePage} layoutProps={{ stickyFooter: false }} />
   </Providers>
 );
