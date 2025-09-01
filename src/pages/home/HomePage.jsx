a6aa04d181ea9d390391c759041fb48225654f17
J-mung <xorb2298@gmail.com>
2025-09-02 01:23:59 +0900

    버튼 컴포넌트 생성 (스타일 미적용)

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,3 +1,5 @@
+import Button from '@/shared/ui/Button';
+
 /**
  * 임시 홈 화면
  * @returns
@@ -7,6 +9,12 @@ export default function HomePage() {
     <div>
       <h1>홈</h1>
       <p>여기에 페이지 콘텐츠를 배치하세요.</p>
+      <Button
+        label="Hello"
+        onClickHandler={() => {
+          alert('외부에서 주입한 함수, 버튼 클릭 함');
+        }}
+      />
       <div style={{ height: 1200 }} />
     </div>
   );
