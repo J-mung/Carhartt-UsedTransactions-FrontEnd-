aa0da2748893a77f70cdb68cab18c14f2cfe28ff
J-mung <xorb2298@gmail.com>
2025-09-04 01:54:11 +0900

    예시용 아이콘 버튼 실사용

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import Button from '@/shared/ui/Button';
-import IconButton from '@/shared/ui/IconButton';
-
+import { IconButton } from '@/shared/ui/icon_button';
+import SearchButton from './ui/SearchButton';
 /**
  * 임시 홈 화면
  * @returns
@@ -16,13 +16,22 @@ export default function HomePage() {
           alert('외부에서 주입한 함수, 버튼 클릭 함');
         }}
       />
+      {/* shared/ui에 선언된 버튼 */}
       <IconButton
+        iconClass={'ic-comment'}
         onClick={() => {
           alert('아이콘 버튼 클릭');
         }}
         title={'test'}
-        shape={'circle'}
+        className={'secondary'}
       />
+      {/* (예시) 홈페이지에서만 사용할 버튼 */}
+      <SearchButton
+        onClick={() => {
+          alert('외부 주입 메서드');
+        }}
+      />
+
       <div style={{ height: 1200 }} />
     </div>
   );
