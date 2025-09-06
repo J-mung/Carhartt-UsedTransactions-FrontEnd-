2137d08bb3137cf16cb03b2ee2b4017299908570
TAEGYU_JANG <57756079+J-mung@users.noreply.github.com>
2025-09-06 23:17:58 +0900

    IconButton component (#1)
    * 버튼 컴포넌트 생성 (스타일 미적용)
    
    * 1차 아이콘 추가
    
    * 아이콘 버튼 스타일 세팅
    
    * button 태그 및 아이콘 버튼 스타일 지정
    
    * 아이콘 버튼 테스트
    
    * 아이콘 버튼 수정
    
    * border mixin 추가
    
    * sass 중첩 맵 헬퍼 함수 추가
    
    * 예시용 아이콘 버튼
    
    * 예시용 아이콘 버튼
    
    * 예시용 아이콘 확장  컴포넌트 (예시)
    
    * 예시용 아이콘 버튼 실사용
    
    * cloudflare pages postcss 빌드 오류 수정
    
    * cloudflare pages postcss 배포 에러 수정

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,3 +1,6 @@
+import Button from '@/shared/ui/Button';
+import { IconButton } from '@/shared/ui/icon_button';
+import SearchButton from './ui/SearchButton';
 /**
  * 임시 홈 화면
  * @returns
@@ -7,6 +10,28 @@ export default function HomePage() {
     <div>
       <h1>홈</h1>
       <p>여기에 페이지 콘텐츠를 배치하세요.</p>
+      <Button
+        label="Hello"
+        onClick={() => {
+          alert('외부에서 주입한 함수, 버튼 클릭 함');
+        }}
+      />
+      {/* shared/ui에 선언된 버튼 */}
+      <IconButton
+        iconClass={'ic-comment'}
+        onClick={() => {
+          alert('아이콘 버튼 클릭');
+        }}
+        title={'test'}
+        className={'secondary'}
+      />
+      {/* (예시) 홈페이지에서만 사용할 버튼 */}
+      <SearchButton
+        onClick={() => {
+          alert('외부 주입 메서드');
+        }}
+      />
+
       <div style={{ height: 1200 }} />
     </div>
   );
