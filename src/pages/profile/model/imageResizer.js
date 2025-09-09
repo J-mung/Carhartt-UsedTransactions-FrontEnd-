2443b2e9093bc6d233b4e562a562ebacd01027f5
J-mung <xorb2298@gmail.com>
2025-09-10 01:23:43 +0900

    압축할 때 height는 width를 기준으로 비율 계산해서 적용

--- src/pages/profile/model/imageResizer.js
+++ src/pages/profile/model/imageResizer.js
@@ -1,20 +1,42 @@
 import pica from 'pica';
 
+const MAX_KBYTE = 1024 * 300; // 1KB * 300
+
+/**
+ * 이미지 압축
+ * height는 직접 계산함 (width 기준)
+ */
 export async function resizeImage(
   file,
-  { width = 800, height = 800, format = 'image/webp', quality = 0.9 } = {}
+  { width = 800, format = 'image/webp', quality = 0.9 } = {}
 ) {
   const img = document.createElement('img');
   img.src = URL.createObjectURL(file);
   await img.decode();
 
+  // 비율 계산
+  const aspectRatio = img.height / img.width;
+  const targetWidth = width;
+  const targetHeight = Math.round(targetWidth * aspectRatio);
+
   const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = targetWidth;
+  canvas.height = targetHeight;
 
   await pica().resize(img, canvas);
 
   const blob = await pica().toBlob(canvas, format, quality);
+  let blobTest;
+  for (let _q = quality; _q > 0.7; _q -= 0.1) {
+    blobTest = await pica().toBlob(canvas, format, quality);
+    if (blobTest.size < MAX_KBYTE) {
+      break;
+    }
+  }
+
+  if (blobTest.size > MAX_KBYTE) {
+    throw new Error(`Over size of the image: ${file.name}`);
+  }
 
   URL.revokeObjectURL(img.src);
 
