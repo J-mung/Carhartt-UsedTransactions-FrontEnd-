accd82d26eaeef26d4846a91b7a27b0e7ac59049
J-mung <xorb2298@gmail.com>
2025-09-20 13:23:25 +0900

    간단 로그인 화면

--- src/pages/login/ui/LoginForm.jsx
+++ src/pages/login/ui/LoginForm.jsx
@@ -1,27 +1,41 @@
+import { carHarttApi } from '@/shared/api/axios';
 import { Button } from '@/shared/ui/buttons';
+import './loginForm.scss';
 
 export default function LoginForm() {
   const onKakaoLogin = () => {
     alert('kakao login 요청');
+    carHarttApi({
+      method: 'GET',
+      url: '/v1/auth/login/kakao',
+      withCredentials: true,
+    })
+      .then((response) => {
+        const { data, meta, status, headers } = response;
+        const { authorizeUrl, state } = data.json();
+        sessionStorage.setItem('oauth_state', state);
+        window.location.assign(authorizeUrl);
+      })
+      .catch((error) => {
+        console.error('Fail login via Kakao');
+      });
   };
 
   return (
-    <div className={'login__form'}>
-      <p>로그인 폼입니다.</p>
-      {/* <div className={'btn'} onClick={onKakaoLogin}>
-        <img
-          src={
-            'src/app/assets/images/kakao/login_ko/kakao_login_large_narrow.png'
-          }
-        />
-      </div> */}
-      <Button>
-        <img
-          src={
-            'src/app/assets/images/kakao/login_ko/kakao_login_large_narrow.png'
-          }
-        />
-      </Button>
+    <div className={'login'}>
+      <div className={'login__form'}>
+        <span className={'login__form__label h2'}>로그인 화면 입니다.</span>
+        <div className={'login__form__buttons'}>
+          <div className={'btn'} onClick={onKakaoLogin}>
+            <img
+              src={
+                'src/app/assets/images/kakao/login_ko/kakao_login_medium_wide.png'
+              }
+            />
+          </div>
+          <Button label={'Naver 추가 예정'} disabled={true} />
+        </div>
+      </div>
     </div>
   );
 }
