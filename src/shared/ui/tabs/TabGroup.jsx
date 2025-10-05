350a155a6755201ec01047ef0d85859529f2c57f
J-mung <xorb2298@gmail.com>
2025-10-06 01:32:28 +0900

    TabGroup prop 폴백 적용

--- src/shared/ui/tabs/TabGroup.jsx
+++ src/shared/ui/tabs/TabGroup.jsx
@@ -1,27 +1,43 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Tab from './Tab';
 
-export default function TabGroup({ tabGroup }) {
+export default function TabGroup({ tabGroup, Layout }) {
   const firstEnabledIndex = tabGroup.findIndex((tab) => !tab.disabled);
   const [activeIndex, setActiveIndex] = useState(firstEnabledIndex);
   const [focusIndex, setFocusIndex] = useState(firstEnabledIndex); // focus 상태에서 키보드 선택 처리용 state
 
   const tabRefs = useRef([]);
+  // 폴백 TabGroup
+  const normalizeTabGroup = tabGroup.map((_tab) => {
+    return {
+      key: _tab.key,
+      label: _tab.label?.toString?.() ?? 'NO_LABEL',
+      content: React.isValidElement(_tab.content) ? _tab.content : noContent(),
+      disabled: !!_tab.disabled,
+    };
+  });
 
+  // 폴백 Content
+  const noContent = () => {
+    return <div>Tab의 Content를 정의하세요.</div>;
+  };
+
+  // Tab의 focus 상태 키입력 handler
   const handleKeyDown = (e, index) => {
-    const length = tabGroup.length;
+    const length = normalizeTabGroup.length;
 
     const findNextEnabledIndex = (start, direction) => {
       let newIndex = start;
       for (let i = 0; i < length; i++) {
         newIndex = (newIndex + direction + length) % length; // 순환 이동
-        if (!tabGroup[newIndex].disabled) {
+        if (!normalizeTabGroup[newIndex].disabled) {
           return newIndex;
         }
       }
       return start; // 모두 disabled면 자기 자신 유지
     };
 
+    // focus 상태 우측 방향키 입력
     if (e.key === 'ArrowRight') {
       const nextIndex = findNextEnabledIndex(index, +1);
       setFocusIndex(nextIndex);
@@ -29,6 +45,7 @@ export default function TabGroup({ tabGroup }) {
       e.preventDefault();
     }
 
+    // focus 상태 좌측 방향키 입력
     if (e.key === 'ArrowLeft') {
       const prevIndex = findNextEnabledIndex(index, -1);
       setFocusIndex(prevIndex);
@@ -38,21 +55,33 @@ export default function TabGroup({ tabGroup }) {
 
     if (e.key === 'Enter' || e.key === ' ') {
       // Enter로 확정할 때도 disabled 예외 처리
-      if (!tabGroup[index].disabled) {
+      if (!normalizeTabGroup[index].disabled) {
         setActiveIndex(index);
       }
       e.preventDefault();
     }
   };
 
+  // 클릭 가능한 탭이 1개라도 존재하는지 확인
   const isActive = () => {
     return activeIndex > -1;
+  };
+
+  // layout 적용
+  const renderContent = () => {
+    if (!isActive) {
+      return <div>출력할 화면이 없습니다.</div>;
+    }
+
+    const ActiveContent = normalizeTabGroup[activeIndex].content;
+    // layout이 확인되면 감싸서 반환
+    return Layout ? <Layout>{ActiveContent}</Layout> : ActiveContent;
   };
 
   return (
     <div className="tab-group">
       <div className="tab-group__list">
-        {tabGroup.map((tab, index) => (
+        {normalizeTabGroup.map((tab, index) => (
           <Tab
             key={tab.key}
             tab={tab}
@@ -66,11 +95,7 @@ export default function TabGroup({ tabGroup }) {
         ))}
       </div>
       <div className="tab-group__content" role="tabpanel">
-        {isActive() ? (
-          tabGroup[activeIndex].content
-        ) : (
-          <div>출력할 화면이 없습니다.</div>
-        )}
+        {renderContent()}
       </div>
     </div>
   );
