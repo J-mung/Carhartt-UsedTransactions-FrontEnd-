006611fd31477bcc9612f396fcf35d22d40e011b
J-mung <xorb2298@gmail.com>
2025-10-07 23:05:48 +0900

    우측 버튼 그룹 추가

--- src/widgets/header/Header.jsx
+++ src/widgets/header/Header.jsx
@@ -1,4 +1,3 @@
-import ThemeToggle from '@/shared/ui/ThemeToggle';
 import Button from '@/shared/ui/buttons/Button';
 import { useNavigate } from 'react-router-dom';
 
@@ -18,7 +17,57 @@ export default function Header({ title }) {
     sessionStorage.removeItem('oauth_state');
     navigate('/');
   };
-  const isLogined = sessionStorage.getItem('oauth_state') === 'Authorized';
+  // const isLogined = sessionStorage.getItem('oauth_state') === 'Authorized';
+  // 쿠키에서 session id get
+  const getJSessionId = () => {
+    var jsId = document.cookie.match(/JSESSIONID=[^;]+/);
+    if (jsId != null) {
+      if (jsId instanceof Array) jsId = jsId[0].substring(11);
+      else jsId = jsId.substring(11);
+    }
+    return jsId;
+  };
+
+  const isLogined = getJSessionId();
+
+  const rightBtnGroup = [
+    {
+      label: '판매하기',
+      variant: 'standard-link',
+      onClick: () => {
+        alert('판매하기 버튼 클릭');
+      },
+      disabled: false,
+    },
+    {
+      label: '관리',
+      variant: 'standard-link',
+      onClick: () => {
+        alert('관리 버튼 클릭');
+      },
+      disabled: false,
+      children: (
+        <>
+          <span className={'ic-user'}></span>
+          <span className={'btn__label text-regular'}>관리</span>
+        </>
+      ),
+    },
+    {
+      label: '채팅',
+      variant: 'standard-link',
+      onClick: () => {
+        alert('채팅 버튼 클릭');
+      },
+      disabled: true,
+    },
+    {
+      label: isLogined ? '로그아웃' : '로그인',
+      variant: 'standard-primary',
+      onClick: isLogined ? handleLogOut : handleLogin,
+      disabled: false,
+    },
+  ];
   return (
     <>
       <Button
@@ -26,13 +75,43 @@ export default function Header({ title }) {
         variant={'standard-link'}
         onClick={handleHome}
       ></Button>
-      {isLogined ? (
-        <Button label={'로그아웃'} onClick={handleLogOut}></Button>
-      ) : (
-        <Button label={'로그인'} onClick={handleLogin}></Button>
-      )}
 
-      <ThemeToggle />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(4, 1fr)',
+          gap: '2px',
+        }}
+      >
+        {/* <Button
+          label={'판매하기'}
+          onClick={() => alert('판매하기 버튼 클릭')}
+        ></Button>
+        <Button label={'관리'} onClick={() => alert('관리 버튼 클릭')}></Button>
+        <Button
+          label={'채팅'}
+          onClick={() => alert('채팅 버튼 클릭')}
+          disabled={true}
+        ></Button>
+        {isLogined ? (
+          <Button label={'로그아웃'} onClick={handleLogOut}></Button>
+        ) : (
+          <Button label={'로그인'} onClick={handleLogin}></Button>
+        )} */}
+        {rightBtnGroup.map((_btn) => {
+          return (
+            <Button
+              label={_btn.label}
+              variant={_btn.variant}
+              onClick={_btn.onClick}
+              disabled={_btn.disabled}
+            >
+              {_btn.children ? _btn.children : ''}
+            </Button>
+          );
+        })}
+        {/* <ThemeToggle /> */}
+      </div>
     </>
   );
 }
