aa9f2ea4cd3800362a6030a97e1974d8813781e2
J-mung <xorb2298@gmail.com>
2025-10-10 23:00:12 +0900

    환경 변수 주입 테스트

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,5 +2,7 @@ import { router as defaultRouter } from '@/app/router.jsx';
 import { RouterProvider } from 'react-router-dom';
 
 export default function App({ router = defaultRouter }) {
+  console.log('BASE_URL' + import.meta.env.VITE_API_BASE_URL);
+
   return <RouterProvider router={router} />;
 }
