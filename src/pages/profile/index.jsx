4a5fd1817a14b317c4835c55748f005ba7304451
J-mung <xorb2298@gmail.com>
2025-10-10 14:34:37 +0900

    Modal 컴포넌트 테스트

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,8 +1,13 @@
+import { Button } from '@/shared/ui/buttons';
 import TabGroup from '@/shared/ui/tabs/TabGroup';
+import Modal from '@/widgets/modal/Modal';
+import { useModal } from '@/widgets/modal/ModalRoot';
 import HomePage from '../home/HomePage';
 import ImageUploader from './ui/ImageUploader';
 
 export default function TestProfilePage() {
+  const { openModal } = useModal();
+
   // 외부에서 정의한 Layout 컴포넌트
   function CardLayout({ children }) {
     return (
@@ -19,6 +24,38 @@ export default function TestProfilePage() {
       </div>
     );
   }
+  const handleOpenInnerTestModal = () => {
+    openModal(Modal, {
+      title: '중첩 모달 테스트',
+      children: <p>중첩 모달 테스트 중입니다.</p>,
+    });
+  };
+  const modalTemplate = (
+    <>
+      <span className={'text-regular'}>
+        테스트 모달입니다. 추가 모달을 열 수도 있고, 현재 모달을 닫을 수도
+        있습니다.
+      </span>
+      <Button
+        label={'추가 모달'}
+        variant={'standard-secondary'}
+        onClick={handleOpenInnerTestModal}
+      />
+    </>
+  );
+  const handleOpenTestModal = () => {
+    openModal(Modal, {
+      title: '테스트 모달',
+      children: modalTemplate,
+      buttons: [
+        {
+          label: '확인',
+          variant: 'standard-primary',
+          onClick: () => console.log('확인 버튼 클릭'),
+        },
+      ],
+    });
+  };
 
   const tabGroup = [
     {
@@ -45,6 +82,11 @@ export default function TestProfilePage() {
     <div>
       <p>router 테스트를 위한</p>
       <p>임시 페이지 입니다.</p>
+      <Button
+        label={'테스트 모달 열기'}
+        variant={'primary-standard'}
+        onClick={handleOpenTestModal}
+      />
       <ImageUploader />
       <TabGroup tabGroup={tabGroup} Layout={CardLayout} />
     </div>
