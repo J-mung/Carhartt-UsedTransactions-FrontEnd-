407b90a58e88430bb84138555ea4a61dcc97b6bf
J-mung <xorb2298@gmail.com>
2025-10-19 22:17:20 +0900

    import 경로 수정

--- src/pages/payment/index.jsx
+++ src/pages/payment/index.jsx
@@ -1,4 +1,5 @@
-import PaymentForm from '@/pages/payment/ui/PaymentForm';
+// 확장자 생략 시, CloudFlare 배포 환경에서 에러 발생
+import PaymentForm from '@/pages/payment/ui/PaymentForm.jsx';
 export default function PaymentPage() {
   return <PaymentForm />;
 }
