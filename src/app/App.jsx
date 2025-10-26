97c65feea45c6cc2b6afc6b366fefdd342e75bbf
TAEGYU_JANG <57756079+J-mung@users.noreply.github.com>
2025-10-26 17:12:55 +0900

    :wrench:로컬 https 프로토콜 세팅 (#20)
    * 로컬 https 프로토콜 적용
    
    * 프록시 쿠키를 로컬에 저장
    
    * 로그인 api 스펙 변경
    
    * 로컬 호스트 alias 추가 및 api 경로 설정 수정
    
    * 로그인 상태 확인 훅 추가
    
    * 유저 상태 hook 추가
    
    * 유저 로그인 여부 확인 hook 추가 및 로그인 여부 서버 응답 처리
    
    * 로그인 여부에 따라 로그인/로그아웃 출력 처리
    
    * 개발 환경 API_BASE_URL 변경

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import { router as defaultRouter } from '@/app/router.jsx';
-import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
 
 // Create a client
 const queryClient = new QueryClient({
