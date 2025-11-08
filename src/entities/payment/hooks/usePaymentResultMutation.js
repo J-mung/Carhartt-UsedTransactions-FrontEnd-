703e85686e93f9203136a321d218d99f63f7aebf
J-mung <xorb2298@gmail.com>
2025-11-08 16:10:16 +0900

    결제 완료 상품 정보 mutation hook 추가

---
+++ src/entities/payment/hooks/usePaymentResultMutation.js
@@ -0,0 +1,38 @@
+import AlternativeImage from '@/app/assets/images/AlternativeImage.jpg';
+import { carHarttApi } from '@/shared/api/axios';
+import { useMockToggle } from '@/shared/config/MockToggleProvider';
+import { useMutation } from '@tanstack/react-query';
+
+// GET /v1/order/{orderId}/item
+export const usePaymentResultMutation = () => {
+  const { useMock } = useMockToggle();
+
+  return useMutation({
+    mutationFn: async ({ orderId }) => {
+      // mock 데이터 사용 시
+      if (useMock) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return {
+          itemId: 1,
+          itemName: '근면성실한 샐러리',
+          itemPrice: 95000,
+          itemImg: AlternativeImage,
+          addressDetail: '서울특별시 강남구 역삼동 ㅁㅁㅁ-ㅁㅁㅁ',
+          orderDateTime: Date.now(),
+        };
+      }
+
+      try {
+        // 응답
+        const response = await carHarttApi({
+          method: 'GET',
+          url: `/v1/order/${orderId}/item`,
+        });
+
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+    },
+  });
+};
