4348f82601df01a4a1e1ff99f12c08998ab89ec0
J-mung <xorb2298@gmail.com>
2025-11-08 16:09:04 +0900

    주문 생성 mutation hook mock 데이터 처리 추가

--- src/entities/order/hooks/useOrderMutation.js
+++ src/entities/order/hooks/useOrderMutation.js
@@ -1,16 +1,31 @@
 import { carHarttApi } from '@/shared/api/axios';
+import { useMockToggle } from '@/shared/config/MockToggleProvider';
 import { useMutation } from '@tanstack/react-query';
 
+// POST /v1/order
 export const useOrderMutation = () => {
+  const { useMock } = useMockToggle();
+
   return useMutation({
     mutationFn: async (payload) => {
-      const response = await carHarttApi({
-        method: 'POST',
-        url: '/v1/order',
-        data: payload,
-      });
+      // mock 데이터 사용 시
+      if (useMock) {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        return { order_id: 1 };
+      }
 
-      return response.data;
+      try {
+        // 응답
+        const response = await carHarttApi({
+          method: 'POST',
+          url: '/v1/order',
+          data: payload,
+        });
+
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
     },
   });
 };
