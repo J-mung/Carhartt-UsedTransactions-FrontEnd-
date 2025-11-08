602a20551cf5cf59c5eb48756a4211061f3a20b0
J-mung <xorb2298@gmail.com>
2025-11-08 16:10:04 +0900

    결제 승인 요청 mutation hook 추가

---
+++ src/entities/payment/hooks/usePaymentApproveMutation.js
@@ -0,0 +1,39 @@
+// /v1/order/payment/approve
+
+import { carHarttApi } from '@/shared/api/axios';
+import { useMockToggle } from '@/shared/config/MockToggleProvider';
+import { useMutation } from '@tanstack/react-query';
+
+// {provider: KAKAOPAY / NAVERPAY, pgToken: String}
+export const usePaymentApproveMutation = () => {
+  const { useMock } = useMockToggle();
+
+  return useMutation({
+    mutationFn: async ({ provider, pgToken }) => {
+      // mock 데이터 사용 시
+      if (useMock) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return { order_id: 1 }; // 테스트 목적으로 order_id를 파라미터로 받아 와야 하나
+      }
+
+      // 요청 Body
+      const requestBody = {
+        provider: provider,
+        pgToken: pgToken,
+      };
+
+      try {
+        // 응답
+        const response = await carHarttApi({
+          method: 'POST',
+          url: '/v1/order/payment/approve',
+          data: requestBody,
+        });
+
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+    },
+  });
+};
