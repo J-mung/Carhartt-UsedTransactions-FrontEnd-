41a51ec68ba063e7afc2f92418cbb1748f51e3e5
J-mung <xorb2298@gmail.com>
2025-11-08 16:09:48 +0900

    결제 준비 mutation hook mock 데이터 처리 추가 및 redirect url 수정

--- src/entities/payment/hooks/usePaymentReadyMutation.js
+++ src/entities/payment/hooks/usePaymentReadyMutation.js
@@ -1,24 +1,42 @@
 import { carHarttApi } from '@/shared/api/axios';
+import { useMockToggle } from '@/shared/config/MockToggleProvider';
 import { useMutation } from '@tanstack/react-query';
 
+// POST /v1/order/${orderId}/payment/ready
 export const usePaymentReadyMutation = () => {
+  const { useMock } = useMockToggle();
+
   return useMutation({
     mutationFn: async ({ orderId, paymentMethod, amount }) => {
+      // mock 데이터 사용 시
+      if (useMock) {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        return {
+          next_redirect_pc_url: `/payment/result?provider=${paymentMethod}&orderid=${orderId}&pg_token=pg123`,
+        };
+      }
+
+      // 요청 Body
       const requestBody = {
         order_id: orderId,
         payment_method: paymentMethod,
         amount_of_payment: amount,
-        approve_url: 'https://localhost:5173/payment/1101/result',
-        fail_url: 'https://localhost:5173/payment/1101/result',
-        cancel_url: 'https://localhost:5173/payment/1101/result',
+        approve_url: `https://localhost:5173/payment/result?provider=${paymentMethod}&orderid=${orderId}`,
+        fail_url: `https://localhost:5173/payment/result?provider=${paymentMethod}&orderid=${orderId}`,
+        cancel_url: `https://localhost:5173/payment/result?provider=${paymentMethod}&orderid=${orderId}`,
       };
-      const response = await carHarttApi({
-        method: 'POST',
-        url: `/v1/order/${orderId}/payment/ready`,
-        data: requestBody,
-      });
+      try {
+        // 응답
+        const response = await carHarttApi({
+          method: 'POST',
+          url: `/v1/order/${orderId}/payment/ready`,
+          data: requestBody,
+        });
 
-      return response.data;
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
     },
   });
 };
